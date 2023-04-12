915117a2381926d736362540586d515ae9e60dd1
Nicholas <nicholas@lanars.com>
2023-04-12 14:46:57 +0300

    MAR-7_MAR-8 Add custom hooks

---
+++ src/hooks/useInfoAnimationVariables.ts
@@ -0,0 +1,189 @@
+import { Periods, TRANSITIONS } from '../constants';
+import { periodsData } from '../data';
+
+const useInfoAnimationVariables = (activePeriodName: Periods) => {
+  const infoAnimationVariablesSlideFromRight = {
+    mainInitial: {
+      x: '100%',
+    },
+    mainAnimate: {
+      x: 0,
+      transition: {
+        x: {
+          delay: TRANSITIONS.DELAY.slide,
+          duration: TRANSITIONS.DURATION.slide,
+        },
+      },
+    },
+    mainExit: {
+      x: '100%',
+      backgroundColor: periodsData[activePeriodName].primaryColor,
+      transition: {
+        x: {
+          delay: TRANSITIONS.DELAY.slide,
+          duration: TRANSITIONS.DURATION.slide,
+        },
+        backgroundColor: {
+          delay: TRANSITIONS.DELAY.backgroundColor,
+          duration: TRANSITIONS.DURATION.backgroundColor,
+        },
+      },
+    },
+    contentBgExit: {
+      backgroundColor: periodsData[activePeriodName].secondaryColor,
+      transition: {
+        backgroundColor: {
+          delay: TRANSITIONS.DELAY.backgroundColor,
+          duration: TRANSITIONS.DURATION.backgroundColor,
+        },
+      },
+    },
+    textInitial: {
+      x: '-100%',
+      opacity: 0,
+    },
+    textAnimate: (delay: number) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay,
+      },
+    }),
+    textExit: (delay: number) => ({
+      opacity: 0,
+      x: '-100%',
+      transition: {
+        duration: 0.3,
+        delay: Math.abs(delay - 1.1),
+      },
+    }),
+  };
+
+  const infoAnimationVariablesSlideFromLeft = {
+    mainInitial: {
+      x: '-100%',
+    },
+    mainAnimate: {
+      x: 0,
+      transition: {
+        x: {
+          delay: TRANSITIONS.DELAY.slide,
+          duration: TRANSITIONS.DURATION.slide,
+        },
+      },
+    },
+    mainExit: {
+      x: '100%',
+      transition: {
+        x: {
+          delay: TRANSITIONS.DELAY.slide,
+          duration: TRANSITIONS.DURATION.slide,
+        },
+        backgroundColor: {
+          delay: TRANSITIONS.DELAY.backgroundColor,
+          duration: TRANSITIONS.DURATION.backgroundColor,
+        },
+      },
+    },
+    contentBgExit: {
+      clipPath: 'inset(0 100% 0 0)',
+      transition: {
+        backgroundColor: {
+          delay: TRANSITIONS.DELAY.backgroundColor,
+          duration: TRANSITIONS.DURATION.backgroundColor,
+        },
+        clipPath: {
+          duration: 0.5,
+        },
+      },
+    },
+    textInitial: {
+      x: '-100%',
+      opacity: 0,
+    },
+    textAnimate: (delay: number) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay,
+      },
+    }),
+    textExit: (delay: number) => ({
+      x: '-100%',
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+        delay: Math.abs(delay - 1.1),
+      },
+    }),
+  };
+
+  const infoAnimationVariablesSlideFromTop = {
+    mainInitial: {
+      y: '-100%',
+    },
+    mainAnimate: {
+      y: 0,
+      transition: {
+        y: {
+          delay: TRANSITIONS.DELAY.slide,
+          duration: TRANSITIONS.DURATION.slide,
+        },
+      },
+    },
+    mainExit: {
+      y: '100%',
+      transition: {
+        y: {
+          delay: TRANSITIONS.DELAY.slide,
+          duration: TRANSITIONS.DURATION.slide,
+        },
+        backgroundColor: {
+          delay: TRANSITIONS.DELAY.backgroundColor,
+          duration: TRANSITIONS.DURATION.backgroundColor,
+        },
+      },
+    },
+    contentBgExit: {
+      clipPath: 'inset(0 0 100% 0)',
+      transition: {
+        backgroundColor: {
+          delay: TRANSITIONS.DELAY.backgroundColor,
+          duration: TRANSITIONS.DURATION.backgroundColor,
+        },
+        clipPath: {
+          duration: 0.5,
+        },
+      },
+    },
+    textInitial: {
+      y: '-100%',
+      opacity: 0,
+    },
+    textAnimate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        delay: 1,
+      },
+    },
+    textExit: {
+      y: '-100%',
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
+  return [
+    infoAnimationVariablesSlideFromRight,
+    infoAnimationVariablesSlideFromLeft,
+    infoAnimationVariablesSlideFromTop,
+  ];
+};
+
+export default useInfoAnimationVariables;
