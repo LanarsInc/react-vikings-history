d96844d8a6b3530f97f464cc4f61e010595a5b1f
Nicholas <nicholas@lanars.com>
2023-04-14 11:36:00 +0300

    MAR-7_MAR-8 Disable some eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,5 +32,8 @@ module.exports = {
                 unnamedComponents: 'arrow-function',
             },
         ],
+        'jsx-a11y/no-static-element-interactions': 0,
+        'jsx-a11y/click-events-have-key-events': 0,
+        'jsx-a11y/no-noninteractive-element-interactions': 0,
     }
 }
