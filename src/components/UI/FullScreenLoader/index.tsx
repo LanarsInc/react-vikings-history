cbb6e7cef89cc6cf3de29dc3bf3f6aecb926e810
Nicholas <nicholas@lanars.com>
2023-04-27 16:17:08 +0300

    MAR-7_MAR-8 Change loader and tab icon

--- src/components/UI/FullScreenLoader/index.tsx
+++ src/components/UI/FullScreenLoader/index.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
 import { motion as m } from 'framer-motion';
-import { ReactComponent as WheelIcon } from '../../../assets/icons/wheel.svg';
+import clsx from 'clsx';
+import { ReactComponent as WaveIcon } from '../../../assets/icons/wave.svg';
+import { ReactComponent as ShipIcon } from '../../../assets/icons/ship.svg';
 import './FullScreenLoader.scss';
 
 const FullScreenLoader: FC = () => {
@@ -10,22 +12,55 @@ const FullScreenLoader: FC = () => {
       transition={{ duration: 0.5, delay: 0.3 }}
       className="full-screen-loader"
     >
-      <m.div
-        animate={{
-          scale: [1, 1.5, 1, 1.5, 1],
-          rotate: [0, 0, 180, 180, 0],
-        }}
-        transition={{
-          duration: 2,
-          ease: 'easeInOut',
-          times: [0, 0.2, 0.5, 0.8, 1],
-          repeat: Infinity,
-          repeatDelay: 0.5,
-        }}
-        className="full-screen-loader__svg-wrapper"
-      >
-        <WheelIcon className="full-screen-loader__svg" />
-      </m.div>
+      <div className="full-screen-loader__content">
+        <m.div
+          animate={{
+            rotate: [0, -6, 0],
+          }}
+          transition={{
+            duration: 1,
+            ease: 'easeInOut',
+            repeat: Infinity,
+          }}
+          className="full-screen-loader__ship-container"
+        >
+          <ShipIcon className="full-screen-loader__ship" />
+          {['left', 'right'].map((paddlePosition) => {
+            return (
+              <m.div
+                key={paddlePosition}
+                animate={{
+                  height: [45, 45, 45, 45, 30, 25, 10, 10, 45, 45],
+                  rotate: [-65, -45, 0, 45, 65, 85, 45, -65, -85, -65],
+                  transformOrigin: 'top',
+                }}
+                transition={{
+                  duration: 1,
+                  ease: 'linear',
+                  repeat: Infinity,
+                }}
+                className={clsx('full-screen-loader__paddle', {
+                  [paddlePosition]: true,
+                })}
+              />
+            );
+          })}
+        </m.div>
+
+        <m.div
+          animate={{
+            translateX: [-1, -103],
+          }}
+          transition={{
+            duration: 1,
+            ease: 'linear',
+            repeat: Infinity,
+          }}
+          className="full-screen-loader__wave-container"
+        >
+          <WaveIcon className="full-screen-loader__wave" />
+        </m.div>
+      </div>
     </m.div>
   );
 };
